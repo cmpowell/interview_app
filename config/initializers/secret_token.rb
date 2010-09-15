77b9bfd55c98ad23a2b52eb8ed16c9dc02a528be
Catherine Powell <cpowell@penitentes.local>
2010-09-15 16:14:21 -0400

    Empty app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InterviewApp::Application.config.secret_token = '8bfdc5d3c0c83f9d6375fa637285e4bfe6483cd06950c3a895ff65294dcd05b140957c43e22b0540805e4e11ade1db9cf4b9e33d3a446ffda29f297e410c6127'

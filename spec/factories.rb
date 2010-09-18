d3a9f74c2130704395055d496593b655773598b9
Catherine Powell <cpowell@penitentes.local>
2010-09-18 15:32:32 -0400

    Show users

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Ronald Spork"
+  user.email                 "rspork@example.com"
+  user.password              "spork4ever"
+  user.password_confirmation "spork4ever"
+end
